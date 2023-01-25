5682561c6d1889a3cc9fd0f997811bdc791434f5
confused-Techie <dev@lhbasics.com>
2023-01-24 19:07:37 -0800

    Use Pusher for the time being to get something functional

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ async function startServer(id) {
       dbPass: null,
       dbDb: process.env.DB_DB,
       dbPort: process.env.DB_PORT,
-      dbSslCert: null
+      dbSslCert: null,
+      pusherAppId: process.env.PUSHER_APP_ID,
+      pusherKey: process.env.PUSHER_KEY,
+      pusherSecret: process.env.PUSHER_SECRET,
+      pusherCluster: process.env.PUSHER_CLUSTER
     });
   } else {
 
